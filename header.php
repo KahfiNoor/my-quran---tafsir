59ac99fc81d031504db074eb8b9aa2d808c93433
KahfiNoor <kahfinoor013@gmail.com>
2024-06-16 21:21:43 +0800

    Update index

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Al Qaara'ah - Qur'an & Tafsir Bahasa Indonesia</title>
+    <title>Al Quran - Qur'an & Tafsir Bahasa Indonesia</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Dark Mode CSS -->
@@ -15,7 +15,7 @@
 
 <body class="mode dark-mode">
     <div class="mt-0 p-3 content text-grey text-center">
-        <h1 class="jumbo">Al Qaara'ah</h1>
+        <h1 class="jumbo">Al Quran</h1>
         <p>Al-Qur'an dan Tafsir Berbahasa Indonesia Lengkap</p>
     </div>
     
